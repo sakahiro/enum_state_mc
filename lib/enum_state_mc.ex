51a9d0fa42a8b2c40feaf652aa54bf20b249e22f
hiroto sakakima <h.s.bbjss383@gmail.com>
2017-04-13 15:51:55 +0900

    Initial commit

---
+++ lib/enum_state_mc.ex
@@ -0,0 +1,18 @@
+defmodule EnumStateMc do
+  @moduledoc """
+  Documentation for EnumStateMc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EnumStateMc.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/enum_state_mc_test.exs
@@ -0,0 +1,8 @@
+defmodule EnumStateMcTest do
+  use ExUnit.Case
+  doctest EnumStateMc
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
